c217c416a0eae72d1654600245960daa7f65be83
natto <natto@natto-tpy.none>
2015-06-02 18:54:58 -0400

    working html displayer window

--- main.js
+++ main.js
@@ -19,4 +19,32 @@ app.on('ready', function() {
     mainwindow.on('closed', function() {
         mainwindow = null;
     });
+
+    var http = require("http");
+    var fs = require("fs");
+    http.createServer(function(req, resp) {
+        resp.writeHead(200, {"Content-Type": "text/html"});
+        if(req.method == "POST") {
+            var body = "";
+            req.on("data", function(data) {
+                body += data;
+            });
+            req.on("end", function() {
+                if(body.match(/html=/)) {
+                    var output = body.substring(5);
+                    var ofilename = "log/" + new Date().getTime() + ".html";
+                    console.log(ofilename);
+                    fs.writeFile(ofilename, output, function(err) {
+                        if(err) {
+                            return console.log("ERROR IN WRITING FILE " + ofilename);
+                        }
+                        mainwindow.loadUrl('file://'+__dirname+'/'+ofilename);
+                    });
+                }
+                resp.end("OK!");
+            });
+        } else {
+            resp.end("send me a post request please");
+        }
+    }).listen(9999);
 });
